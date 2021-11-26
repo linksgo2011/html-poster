3bbb60831a699e6208e38b6ac7f31239e88da967
lin <120377843@qq.com>
2021-11-26 11:05:01 +0800

    feat: update default template

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# poster-by-html
+# html-poster
 A poster maker by html template.
+
+## usage
+
+> git clone https://github.com/linksgo2011/poster-by-html.git
+
+> cd poster-by-html
+
+> node bin/index.js -t default -p '{"author":"少个分号","content":"架构的原则：权衡大于绝对；合适大于正确；共识大于偏好。"}'
+
+or 
+
+> npm install -g .
+
+> htmlposter -t default -p '{"author":"少个分号","content":"架构的原则：权衡大于绝对；合适大于正确；共识大于偏好。"}'
+
+## reference
+
+- https://ejs.co/#install
+- command-line-app-with-nodejs https://developer.okta.com/blog/2019/06/18/command-line-app-with-nodejs
--- bin/index.js
+++ bin/index.js
@@ -4,43 +4,77 @@ const puppeteer = require('puppeteer');
 const fs = require("fs");
 const ejs = require("ejs");
 
-const options = yargs
- .usage("Usage: -t <template> -p <prameters> -o <output>")
- .option("t", { alias: "template", describe: "template name", type: "string", demandOption: true })
-  .option("p", { alias: "prameters", describe: "prameters for template", type: "json", demandOption: false })
-  .option("o", { alias: "output", describe: "output name, default is 'output.png'", type: "string", demandOption: false })
- .argv;
-
-console.log(`You are creating poster by template: ${options.template}`);
-console.log(options)
-
-const rootDir = process.cwd();
-
-// 1. compile file 
+// 0 parse template
+// 1. compile file
 // 2. output html
 // 3. screenshot
-// 4. clear 
+// 4. clear
 
+const options = yargs
+    .usage("Usage: -t <template> -p <prameters> -o <output>")
+    .option("t", {alias: "template", describe: "template name", type: "string", demandOption: true})
+    .option("p", {alias: "prameters", describe: "prameters for template", type: "string", demandOption: false})
+    .option("o", {
+        alias: "output",
+        describe: "output name, default is 'output.png'",
+        type: "string",
+        demandOption: false
+    })
+    .argv;
 
-const templateFile = `/${rootDir}/templates/${options.template}.html`;
+console.log(`You are creating poster by template: ${options.template}`);
+
+const rootDir = process.cwd();
+const templateDir = `${rootDir}/templates/${options.template}/`;
+if (!fs.existsSync(templateDir)) {
+    console.error("Template not found!");
+    process.exit(1);
+    return;
+}
+const metaJson = fs.readFileSync(`${templateDir}/meta.json`).toString();
+const templateFile = `${templateDir}index.html`;
 const templateString = fs.readFileSync(templateFile).toString();
-const redenered = ejs.render(templateString, JSON.parse(options.prameters), {});
 
-const outputHtmlPath = `/${rootDir}/templates/tmp.html`;
-fs.writeFileSync(outputHtmlPath,redenered);
+const data = {
+    sys: presetData(),
+    ...JSON.parse(options.prameters)
+}
+const rendered = ejs.render(templateString, data, {});
 
-const outputHtmlUrl = `file:/${outputHtmlPath}`;
+const outputHtmlPath = `${templateDir}tmp.html`;
+fs.writeFileSync(outputHtmlPath, rendered);
+
+const outputHtmlUrl = `file://${outputHtmlPath}`;
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  page.setViewport({
-  width: 800,
-  height: 1150,
-  deviceScaleFactor: 1,
-});
-  const output = options.output?options.output:'output.png';
-  await page.goto(outputHtmlUrl);
-  await page.screenshot({ path: 'output.png' });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.setViewport({
+        width: 800,
+        height: 1150,
+        deviceScaleFactor: 1,
+        ...JSON.parse(metaJson)
+    });
+    const output = options.output ? options.output : 'output.png';
+    await page.goto(outputHtmlUrl);
+    await page.screenshot({path: output});
 
-  await browser.close();
+    await browser.close();
+    // fs.rmSync(outputHtmlUrl);
 })();
+
+
+function presetData() {
+    const now = new Date();
+    const nowArray = now.toString().split(" ");
+    return {
+        en: {
+            dayInWeek: nowArray[0],
+            month: nowArray[1],
+            dayInMonth: nowArray[2],
+            year: nowArray[3],
+            time: nowArray[4],
+            monthNumber: now.getMonth() + 1
+        },
+        // turn to other
+    }
+}
